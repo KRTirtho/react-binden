68f867ee8118bec21f21a9343c669154adaa928c
KRTirtho <krtirtho@gmail.com>
2021-10-25 13:11:31 +0600

    Form test added

--- test/components/Form.test.tsx
+++ test/components/Form.test.tsx
@@ -1,1 +1,8 @@
 import { Form } from '../../src/components/Form';
+
+test('renders', () => {
+  //   render(<App />);
+  //   const linkElement = screen.getByText(/learn react/i);
+  //   expect(linkElement).toBeInTheDocument();
+  expect(true).toBe(true);
+});
